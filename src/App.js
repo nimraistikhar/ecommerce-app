82a0e203f99451e826aa253fc0061855a57a3056
nimra <nimra@myyogateacher.com>
2022-11-20 12:03:14 +0500

    Latest Product'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Home from './components/home';
 import Navbar from './components/navbar';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
+      <>
+        <Navbar />
+        <Home />
+      </>
     </>
   );
 }
