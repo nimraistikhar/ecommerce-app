e6f6c26587bd8d0f8fb8e643e25ba577c0734c61
nimra <nimra@myyogateacher.com>
2022-11-20 13:07:48 +0500

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import Home from './components/home';
 import Navbar from './components/navbar';
 import { Switch, Route } from 'react-router-dom';
+import Product from './components/product';
 
 function App() {
   return (
     <>
-      <>
-        <Navbar />
-        <Home />
-      </>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products" component={Product} />
+      </Switch>
     </>
   );
 }
