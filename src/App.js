a3a0ff19e607ee2f3e2d2c4daef1c55ad4a3c99e
nimra <nimra@myyogateacher.com>
2022-11-22 01:05:03 +0500

    Add product design and details

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/products" component={Products} />
-        <Route exact path="/product:/id" component={Product} />
+        <Route exact path="/products:/id" component={Product} />
+        <Product />
       </Switch>
     </>
   );
